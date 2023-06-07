fab52071009d0a7631f360628da00e80135c115b
LeeJaeHyeok97 <hazardous10@naver.com>
2023-06-07 20:51:30 +0900

    스프링 jdbc, jpa 사용하기

---
+++ src/main/java/hello/hellospring/repository/MemberRepository1.java
@@ -0,0 +1,34 @@
+package main.java.hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberRepository1 {
+    private MemberRepository repository;
+
+    public MemberRepository() {
+        repository = new MemberRepository() {
+            @Override
+            public Member save(Member member) {
+                return null;
+            }
+
+            @Override
+            public Optional<Member> findById(Long id) {
+                return Optional.empty();
+            }
+
+            @Override
+            public Optional<Member> findByName(String name) {
+                return Optional.empty();
+            }
+
+            @Override
+            public List<Member> findAll() {
+                return null;
+            }
+        };
+    }
+}
