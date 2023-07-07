0c5c77fac7daee6855ab3b22796f97c3f9f6af17
LeeJaeHyeok97 <hazardous10@naver.com>
2023-07-07 19:53:58 +0900

    crud 학습코드

---
+++ crud/src/main/java/com/example/dcInsideClone2/controller/BoardController.java
@@ -0,0 +1,70 @@
+package com.example.dcInsideClone2.controller;
+
+import com.example.dcInsideClone2.dto.BoardDTO;
+import com.example.dcInsideClone2.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) { //데이터 전체를 가져와야 할 때는 Model 객체를 사용한다.
+        //DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        /*
+            해당 게시글의 조회수를 하나 올리고
+            게시글 데이터를 가져와서 detail.html에 출력
+         */
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateForm(@PathVariable Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("boardUpdate", boardDTO);
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        BoardDTO board = boardService.update(boardDTO);
+        model.addAttribute("board", board);
+        return "detail";
+//        return "redirect:/board/" + boardDTO.getId(); 수정 시 조회수가 올라가는 문제 있음
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        boardService.delete(id);
+        return "redirect:/board/";
+    }
+}
---
+++ crud/src/main/java/com/example/dcInsideClone2/service/BoardService.java
@@ -0,0 +1,67 @@
+package com.example.dcInsideClone2.service;
+
+//DTO -> Entity (Entity Class)
+//Entity -> DTO (DTO Class)
+
+import com.example.dcInsideClone2.dto.BoardDTO;
+import com.example.dcInsideClone2.entity.BoardEntity;
+import com.example.dcInsideClone2.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+    public Long save(BoardDTO boardDTO) {
+        BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
+        boardRepository.save(boardEntity);
+        return boardEntity.getId();
+    }
+
+    public List<BoardDTO> findAll() { //엔티티로 받아온 객체를 DTO로 변환해서 옮겨줘야한다.
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        //엔티티 객체를 DTO객체로 옮겨 담는다.
+        for (BoardEntity boardEntity : boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+
+        return boardDTOList;
+    }
+
+    @Transactional //jpa에 수동적으로 추가해준 메서드는 Transactional을 붙여준다.
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+
+    public BoardDTO findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            BoardDTO boardDTO = BoardDTO.toBoardDTO(boardEntity);
+            return boardDTO;
+        } else {
+            return null;
+        }
+    }
+
+    public BoardDTO update(BoardDTO boardDTO) {
+        //jpa에는 업데이트를 위한 메서드가 따로 제공되지 않는다. save 메서드를 활용해서 update, insert를 가능하게 한다.(id 유무로 기능 구분)
+        BoardEntity boardEntity = BoardEntity.toUpdateEntity(boardDTO);
+        boardRepository.save(boardEntity);
+        return findById(boardDTO.getId());
+
+    }
+
+    public void delete(Long id) {
+        boardRepository.deleteById(id);
+    }
+}
